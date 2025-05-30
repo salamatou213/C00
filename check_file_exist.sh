53b62d92b2ff8459e3434c5043de82bddd3e9f52
Salamatou316 <adamouh84@gmail.com>
2025-05-31 00:20:11 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,8 +3,8 @@
 echo "Entrez le nom du fichier :"
 read filename 
 
-if [ "$filename" ]; then
-  echo "Le fichier '$fichier' existe."
+if [ -f "$filename" ]; then
+  echo "Le fichier '$filename' existe."
 else
   echo "Le fichier '$filename' n'existe pas."
 fi
